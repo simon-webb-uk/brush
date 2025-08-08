25cdb375763cdcd80386aa45b73ab1e87b07a59b
Simon Webb <sywebb@hotmail.com>
2025-08-08 16:51:21 +0100

    clean up and build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,3 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { Link } from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 import HomePage from '../src/pages/home';
 
 export default function Home() {
